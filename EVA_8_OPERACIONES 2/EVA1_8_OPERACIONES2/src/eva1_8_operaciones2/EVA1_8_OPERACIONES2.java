c6326698f9d353ce00c1f998fde7c29155b89440
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-09-04 16:54:54 -0600

    Add files via upload

---
+++ EVA_8_OPERACIONES 2/EVA1_8_OPERACIONES2/src/eva1_8_operaciones2/EVA1_8_OPERACIONES2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operaciones2;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA1_8_OPERACIONES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here}
+        String nombre = "Cristian";
+        String apellido = "Reyes";
+        String nombreCom;
+        //CADENA DE TEXTO LA OPERACIONES SE LLAMA
+        //CONCATENACION---)UNIR CADENAS DE  TEXTO
+        //El operador de concatenacion es el + 
+        // cuando simbolo tiene varias funciones
+        //se les llama sobrecarga (de operador)
+       
+        nombreCom = nombre + apellido;
+        System.out.println("nombre es:" + nombre);
+        System.out.println("El apellido es:" + apellido);
+        System.out.println("El nombre completo es:" + nombreCom);
+        
+        
+        
+        
+    }
+    
+}
